556b2a9eaf44597132cb7d4e91cd1c11cc2462d2
ParkSangSun1 <appleasz1028102@gmail.com>
2021-05-26 15:11:22 +0900

    문제 뒤로가기 버튼

--- app/src/main/java/com/what/working_level_android/GeoQuiz/MainActivity.kt
+++ app/src/main/java/com/what/working_level_android/GeoQuiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.what.working_level_android.GeoQuiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -20,34 +21,69 @@ class MainActivity : AppCompatActivity() {
     )
     private var currentIndex = 0
 
-
-    val trueButton = findViewById<Button>(R.id.true_button)
-    val falseButton = findViewById<Button>(R.id.false_button)
-    val nextButton = findViewById<Button>(R.id.next_button)
-    val questionTextView = findViewById<TextView>(R.id.question_text_view)
+    lateinit var questionTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val trueButton = findViewById<Button>(R.id.true_button)
+        val falseButton = findViewById<Button>(R.id.false_button)
+        val nextButton = findViewById<Button>(R.id.next_button)
+        questionTextView = findViewById<TextView>(R.id.question_text_view)
+        val previousButton = findViewById<Button>(R.id.previous_button)
 
-
+        Log.d("로그", "현재 : $currentIndex")
         trueButton.setOnClickListener {
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(true)
         }
         falseButton.setOnClickListener {
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(false)
         }
         updateQuestion()
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
+            nextQuestion()
+
         }
+        questionTextView.setOnClickListener {
+
+            nextQuestion()
+
+        }
+        previousButton.setOnClickListener {
+            if (currentIndex <= 0) {
+                Toast.makeText(this, "더이상 뒤로 갈수 없습니다", Toast.LENGTH_SHORT).show()
+            } else {
+                previousQuestion()
+            }
+        }
+
+    }
+
+    private fun nextQuestion() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+        updateQuestion()
+    }
+
+    private fun previousQuestion() {
+        currentIndex = (currentIndex - 1)
+        updateQuestion()
     }
 
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
 
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
